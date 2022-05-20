089b513d0e70c06136e1ecf2b125c54fecb57740
Guillem Esteve <72156857+esteveguillem@users.noreply.github.com>
2022-05-20 10:26:47 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,8 +39,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8942ee790564be332ec8f017b7ae3bf6",
-"/": "8942ee790564be332ec8f017b7ae3bf6",
+"index.html": "6057bffb38e65e3705bc1677e124a625",
+"/": "6057bffb38e65e3705bc1677e124a625",
 "main.dart.js": "ffe54d6283b01370da7697191fee95a5",
 "manifest.json": "c2e0e590e5f6f8dd7e6f6b676429c749",
 "version.json": "5b60e0d1ed0efaae011545df6c038bb1"
